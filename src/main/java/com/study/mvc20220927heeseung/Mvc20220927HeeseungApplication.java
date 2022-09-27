6ac3f84448cf94b4d1163096226e0af73d5cdd2a
heeseung0 <ys2645@naver.com>
2022-09-27 17:52:06 +0900

    no message

---
+++ src/main/java/com/study/mvc20220927heeseung/Mvc20220927HeeseungApplication.java
@@ -0,0 +1,13 @@
+package com.study.mvc20220927heeseung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvc20220927HeeseungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mvc20220927HeeseungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/mvc20220927heeseung/Mvc20220927HeeseungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.mvc20220927heeseung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvc20220927HeeseungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
